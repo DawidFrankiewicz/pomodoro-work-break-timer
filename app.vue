863de7881cc26a43db527c686a067fb20a628ea6
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-11-25 01:10:17 +0100

    Project setup

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="min-h-screen bg-slate-900 bg-gradient-to-br from-slate-900 to-blue-900 text-white"
+    >
+        <h1 class="pt-60 text-center text-6xl font-bold">Work Brake Timer</h1>
+    </div>
+</template>
