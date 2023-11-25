08627ebf6295b0507f62ec165ff4f0de2ac855a4
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-11-25 01:28:19 +0100

    Added basic timer

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
     <div
-        class="min-h-screen bg-slate-900 bg-gradient-to-br from-slate-900 to-blue-900 text-white"
+        class="min-h-screen bg-slate-900 bg-gradient-to-br from-slate-900 to-blue-900 text-center text-white"
     >
-        <h1 class="pt-60 text-center text-6xl font-bold">Work Brake Timer</h1>
+        <Timer />
     </div>
 </template>
---
+++ components/Timer.vue
@@ -0,0 +1,21 @@
+<script setup>
+const timer = ref(0);
+
+onMounted(() => {
+    setInterval(() => {
+        timer.value++;
+    }, 1000);
+});
+</script>
+
+<template>
+    <div></div>
+    <div class="flex min-h-screen flex-col items-center justify-center">
+        <h1 class="py-6 text-6xl font-bold">Work Brake Timer</h1>
+        <div
+            class="rounded-sm bg-slate-500 bg-opacity-30 px-6 py-5 text-3xl font-bold"
+        >
+            {{ timer }}
+        </div>
+    </div>
+</template>
