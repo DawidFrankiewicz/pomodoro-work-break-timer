0d5783d143166fb3f6c123bcfe32c462974aabcf
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-11-30 15:52:37 +0100

    Added reset button

--- components/TimerControls.vue
+++ components/TimerControls.vue
@@ -19,23 +19,22 @@ const emit = defineEmits(['start-timer', 'stop-timer', 'reset-timer']);
         class="absolute bottom-0 left-1/2 flex h-20 -translate-x-1/2 items-end justify-center px-4 pb-2 pt-4 [perspective:200px]"
     >
         <button
-            @click="
-                isTimerEnded
-                    ? $emit('reset-timer')
-                    : isTimerRunning
-                      ? $emit('stop-timer')
-                      : $emit('start-timer')
-            "
+            @click="$emit('reset-timer')"
             class="group relative z-20 flex origin-bottom items-center px-4 text-purple-950"
         >
             <font-awesome-icon
-                :icon="
-                    isTimerEnded
-                        ? ['fas', 'refresh']
-                        : isTimerRunning
-                          ? ['fas', 'pause']
-                          : ['fas', 'play']
-                "
+                :icon="['fas', 'refresh']"
+                class="pointer-events-none h-12 w-12 origin-bottom transition-transform group-hover:scale-110"
+            />
+        </button>
+        <button
+            :disabled="isTimerEnded"
+            @click="isTimerRunning ? $emit('stop-timer') : $emit('start-timer')"
+            class="group relative z-20 flex origin-bottom items-center px-4 text-purple-950"
+            :class="isTimerEnded ? 'opacity-40' : ''"
+        >
+            <font-awesome-icon
+                :icon="isTimerRunning ? ['fas', 'pause'] : ['fas', 'play']"
                 class="pointer-events-none h-12 w-12 origin-bottom transition-transform group-hover:scale-110"
             />
         </button>
