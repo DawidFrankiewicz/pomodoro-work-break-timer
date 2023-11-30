e7a7e3e7292912428cbc89a701a92f0781b481af
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-11-30 12:44:33 +0100

    Added timer controlls

--- components/Timer.vue
+++ components/Timer.vue
@@ -29,14 +29,14 @@ onUnmounted(() => {
 
 const displayTime = (timeInSeconds, displayHours = false) => {
     const hours = Math.floor(timeInSeconds / 3600);
-    const minutes = Math.floor(timeInSeconds / 60) % 60;
+    const minutes = Math.floor(timeInSeconds / 60);
     const seconds = timeInSeconds % 60;
 
     if (displayHours) {
         return (
             String(hours).padStart(2, '0') +
             ':' +
-            String(minutes).padStart(2, '0') +
+            String(minutes % 60).padStart(2, '0') +
             ':' +
             String(seconds).padStart(2, '0')
         );
@@ -93,55 +93,33 @@ const startWork = () => {
         class="mx-auto flex min-h-screen max-w-[1920px] flex-col items-center justify-center"
     >
         <h1 class="py-6 text-6xl font-bold">Work Brake Timer</h1>
-        <div
-            class="rounded-sm bg-slate-500 bg-opacity-30 px-6 py-5 text-3xl font-bold"
-        >
-            Total: {{ displayTime(totalTimer, true) }}
+        <div class="flex flex-col items-center rounded-sm px-6 py-5">
+            <span class="text-3xl font-bold">
+                {{ displayTime(totalTimer, true) }}
+            </span>
+            <span class="text-9xl font-bold">
+                {{ displayTime(timer) }}
+            </span>
         </div>
-        <div
-            class="rounded-sm bg-slate-500 bg-opacity-30 px-6 py-5 text-3xl font-bold"
-        >
-            {{ (isWorkTime ? 'Work: ' : 'Brake: ') + displayTime(timer, true) }}
-        </div>
-        <div
-            v-if="props.config.breakTime"
-            class="rounded-sm bg-slate-500 bg-opacity-30 px-6 py-5 text-3xl font-bold"
-        >
-            Break time: {{ displayTime(props.config.breakTime) }}
-        </div>
-        <div
-            v-if="props.config.workTime"
-            class="rounded-sm bg-slate-500 bg-opacity-30 px-6 py-5 text-3xl font-bold"
-        >
-            Work time: {{ displayTime(props.config.workTime) }}
-        </div>
+        <div class="rounded-sm px-6 py-5 text-3xl font-bold"></div>
         <div
             v-if="props.config.totalRounds"
-            class="rounded-sm bg-slate-500 bg-opacity-30 px-6 py-5 text-3xl font-bold"
+            class="rounded-sm px-6 py-5 text-3xl font-bold"
         >
             Rounds: {{ props.config.totalRounds }}
         </div>
         <div
             v-if="props.config.isInfinite !== undefined"
-            class="rounded-sm bg-slate-500 bg-opacity-30 px-6 py-5 text-3xl font-bold"
+            class="rounded-sm px-6 py-5 text-3xl font-bold"
         >
             Rounds: {{ props.config.isInfinite }}
         </div>
-        <button
-            @click="isTimerRunning ? stopTimer() : startTimer()"
-            class="bg-red-600 p-4 text-white"
-        >
-            Stop/Start
-        </button>
 
-        <!-- Controls -->
-        <div
-            class="absolute bottom-0 left-1/2 h-20 w-40 -translate-x-1/2 [perspective:200px]"
-        >
-            <div
-                class="absolute h-full w-full origin-bottom rounded-t-xl bg-red-500 [transform:rotateX(30deg)]"
-            ></div>
-        </div>
-        <!-- End Controls -->
+        <TimerControls
+            :isTimerRunning="isTimerRunning"
+            @startTimer="startTimer()"
+            @stopTimer="stopTimer()"
+        />
     </div>
 </template>
+6
---
+++ components/TimerControls.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+    isTimerRunning: {
+        type: Boolean,
+        required: true,
+    },
+});
+
+const emit = defineEmits(['start-timer', 'stop-timer']);
+</script>
+
+<template>
+    <div
+        class="absolute bottom-0 left-1/2 flex h-20 -translate-x-1/2 items-end justify-center px-4 pb-2 pt-4 [perspective:200px]"
+    >
+        <button
+            @click="isTimerRunning ? $emit('stop-timer') : $emit('start-timer')"
+            class="group relative z-20 flex origin-bottom items-center px-4 text-purple-950"
+        >
+            <font-awesome-icon
+                ref="applyButtonIcon"
+                :icon="isTimerRunning ? ['fas', 'pause'] : ['fas', 'play']"
+                class="pointer-events-none h-12 w-12 origin-bottom transition-transform group-hover:scale-110"
+            />
+        </button>
+        <div
+            class="absolute left-0 top-0 h-full w-full origin-bottom rounded-t-xl bg-purple-200 bg-gradient-to-b from-purple-200 to-purple-400 [transform:rotateX(30deg)]"
+        ></div>
+    </div>
+</template>
