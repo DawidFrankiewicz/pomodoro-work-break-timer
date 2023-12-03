887c71be38b5fca1ec7f05eda49cc3121981258d
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-12-03 11:26:30 +0100

    Changed favicon url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,9 @@ export default defineNuxtConfig({
                     content: 'width=device-width, initial-scale=1',
                 },
             ],
-            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '~/favicon.ico' },
+            ],
         },
     },
     devtools: { enabled: true },
