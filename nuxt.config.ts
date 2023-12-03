7e6318713da54ba099df0d5447162d5d55a378a8
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-12-03 10:35:27 +0100

    Removed unused

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,11 +9,6 @@ export default defineNuxtConfig({
                     name: 'viewport',
                     content: 'width=device-width, initial-scale=1',
                 },
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: '',
-                },
             ],
             link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
         },
