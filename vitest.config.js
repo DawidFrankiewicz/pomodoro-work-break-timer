5f89c8ccf1ab4144435e4100e916ad518f5fc3e8
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-12-05 14:05:01 +0100

    Added happy-dom env

--- vitest.config.js
+++ vitest.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import Vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [Vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './'),
+        },
+    },
     test: {
         globals: true,
         coverage: {
             providers: 'v8',
             include: ['components/**', 'utils/**', 'app.vue'],
         },
+        environment: 'happy-dom',
     },
 });
