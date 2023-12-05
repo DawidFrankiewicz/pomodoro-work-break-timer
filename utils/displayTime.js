b96c5378767edb18efb6e0c34d965ff89836a234
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-12-05 12:29:24 +0100

    Added handling negative values for displayTime()

--- utils/displayTime.js
+++ utils/displayTime.js
@@ -1,4 +1,9 @@
 const displayTime = (timeInSeconds, displayHours = false) => {
+    // If time is negative, display a minus sign and the positive time
+    if (timeInSeconds < 0) {
+        return '-' + displayTime(Math.abs(timeInSeconds), displayHours);
+    }
+
     const hours = Math.floor(timeInSeconds / 3600);
     const minutes = Math.floor(timeInSeconds / 60);
     const seconds = timeInSeconds % 60;
