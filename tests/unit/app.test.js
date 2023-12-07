c0e02ee9f99b79908c2ba2dd111e51eb2283bfeb
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-12-07 12:10:47 +0100

    Added auto imports for tests

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -1,3 +1,23 @@
+import { shallowMount, mount } from '@vue/test-utils';
+import Timer from '@/components/Timer.vue';
+import TimerSettings from '@/components/TimerSettings.vue';
+
+const components = {
+    Timer,
+    TimerSettings,
+};
+
 describe('App.test.js', () => {
-    test.todo('TODO');
+    test('Should render correctly', async () => {
+        const component = await import('@/app.vue');
+        expect(component).toBeDefined();
+
+        const wrapper = shallowMount(component.default, {
+            components: components,
+        });
+
+        // Check if component matches snapshot (HTML structure)
+        // https://vitest.dev/guide/snapshot#updating-snapshots
+        expect(wrapper.html()).toMatchSnapshot();
+    });
 });
--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vitest/config';
 import Vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [Vue()],
+    plugins: [
+        Vue(),
+        AutoImport({
+            imports: ['vue'],
+        }),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './'),
